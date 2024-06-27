2d84933a7b6b1948deac7eec956f94b15d63529c
Herison9Bit <herison.molinari@escola.pr.gov.br>
2024-06-27 17:38:31 -0300

    omg a bolinha do enzo ooooow

--- pong.js
+++ pong.js
@@ -1,3 +1,9 @@
+//bolinha
+let xbolinhadoenzo = 300;
+let ybolinhadoenzo = 200;
+let tamboinhaenzo = 25;
+
+
 function setup(){
     //aqui vou criar minha "mesa"
 createCanvas(600,400);
@@ -6,4 +12,21 @@ createCanvas(600,400);
 function draw(){
     //aqui vou por a cor da "mesa"
     background(25,25,112);
+}
+
+// omg bolinha mexe wooooo
+function nossabolinha(xbolinhadoenzo,ybolinhadoenzo,tamboinhadoenzo){
+    circle (xbolinhadoenzo,ybolinhadoenzo,tamboinhadoenzo);
+
+
+
+
+
+
+
+
+
+
+
+
 }
