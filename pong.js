fa6c105bb195f600b4c50f1dbfc82e1e1d158ea3
Herison9Bit <herison.molinari@escola.pr.gov.br>
2024-08-15 16:53:08 -0300

    Update pong.js

--- pong.js
+++ pong.js
@@ -28,11 +28,45 @@ function movebolinha(){
     ybolinhadoenzo = ybolinhadoenzo + yvb
 }
 function kikabolinha(){
-    if (ybolinhadoenzo > width || xbolinhadoenzo < 0){
-        xvb *= -1;
-        
-    }
-    if (xbolinhadoenzo > width || ybolinhadoenzo < 0){
-        yvb *= -1;    
+    if (xBolinha > width || xBolinha < 0){
+        xvelo *= -1;
+        if (xBolinha < 0) {
+          mortes++;
+        }
+        if (yBolinha > height || yBolinha < 0){
+        yvelo *= -1;
+        }
     
+}
+
+
+
+    
+
+function criaRaquete(xRaquete,yRaquete,larguraRaquete,alturaRaquete){
+    fill("green");
+    rect(xRaquete,yRaquete,larguraRaquete,alturaRaquete);
+}
+
+function MovimentaRaquete(){
+  if(keyIsDown(UP_ARROW)){
+      yRaquete -=10;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+      yRaquete +=10;
+  }
+}
+
+function colideRaquete(){
+    if(xBolinha - raiobolinha < xRaquete + larguraRaquete && yBolinha - raiobolinha < yRaquete + alturaRaquete && yBolinha + raiobolinha > yRaquete){
+        xvelo *= -1;
+        pontos++;
+    }
+}
+
+function mostraPlacar() {
+  fill(0);
+  textSize(24);
+  text("Mortes: " + mortes, 10, 30);
+  text("Pontos: " + pontos, 10, 60);
 }
